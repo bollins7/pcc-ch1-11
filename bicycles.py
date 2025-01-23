040b82f16b9c956c10c1f2c0b0bfad240f880f22
bollins7 <ccollins0410@gmail.com>
2025-01-22 19:56:51 -0600

    Create bicycles
    Printing from a list

---
+++ bicycles.py
@@ -0,0 +1,7 @@
+# First item in list is always 0.
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[3])
+
+# Last item on list can be written as -1
