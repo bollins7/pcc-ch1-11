0c548f8b801b61dd9c1b1f975e4b4d0df4f429ef
bollins7 <ccollins0410@gmail.com>
2025-01-22 21:11:25 -0600

    motorcycles.py
    working with lists. append, insert, remove, and pop items in a list

---
+++ motorcycles.py
@@ -0,0 +1,37 @@
+# Modifying Elements in a list
+motorcycles = ['honda', 'yahama', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('harley davidson')
+print(motorcycles)
+
+# Making an empty list and adding to it with append
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('suzuki')
+motorcycles.append('yahama')
+print(motorcycles)
+
+# Inserting elements into a list
+motorcycles = ['yahama', 'honda', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+motorcycles.insert(2, 'harley davidson')
+print(motorcycles)
+
+# Removing an element from a list
+del motorcycles[2]
+print(motorcycles)
+
+# Removing an item using the pop() method. Removes the top of the list but you can still
+# work with it and that is shown by printing the popped_motorcycle 
+motorcycles = ['honda', 'yahama', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
