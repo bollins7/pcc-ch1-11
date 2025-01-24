2b5c6a07619d122b4911e00ee20d5589a278e25a
bollins7 <ccollins0410@gmail.com>
2025-01-23 20:44:30 -0600

    update guest_list.py
    guest list, changing guest list, more guests, and shrinking guest list problem complete

--- guest_list.py
+++ guest_list.py
@@ -1,7 +1,16 @@
-guest_list = ['Bernie', 'Obama', 'Reich']
+guest_list = ['Bernie', 'Obama', 'Reich'] 
 
 del guest_list[0]
 guest_list.append('Fran')
 print(f"{guest_list[0]}! Please come to dinner!")
 print(f"{guest_list[1]}! Please come to dinner!")
 print(f"{guest_list[2]}! Please come to dinner!")
+
+guest_list.insert(0, 'Boy')
+print(guest_list)
+
+guest_list.pop(2)
+print(f"So sorry you can't come {guest_list.pop(2)}")
+
+guest_list.pop(1)
+print(f"So sorry you can't come {guest_list.pop(0)}")
