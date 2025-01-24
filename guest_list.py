0bbf830b7a350f97acf80ce9d93115f2e9f0cac4
bollins7 <ccollins0410@gmail.com>
2025-01-23 20:32:30 -0600

    update
    getting rid of the first prints

--- guest_list.py
+++ guest_list.py
@@ -1,7 +1,4 @@
 guest_list = ['Bernie', 'Obama', 'Reich']
-print(f"{guest_list[0]}! Please come to dinner!")
-print(f"{guest_list[1]}! Please come to dinner!")
-print(f"{guest_list[2]}! Please come to dinner!")
 
 del guest_list[0]
 guest_list.append('Fran')
