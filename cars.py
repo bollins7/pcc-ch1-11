58521a785c7cf93a3b74074b08e05b06593f214d
bollins7 <ccollins0410@gmail.com>
2025-01-26 20:04:56 -0600

    cars.py
    sorting a list temporarily with the sorted() function

--- cars.py
+++ cars.py
@@ -5,3 +5,13 @@ print(cars)
 # sort in reverse
 cars.sort(reverse=True)
 print(cars)
+
+# Sorting a list temporarily with the sorted() function
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
