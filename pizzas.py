8a14e49f93fa2118111cd1e8168059aa2d792e64
bollins7 <ccollins0410@gmail.com>
2025-01-28 10:33:06 -0600

    create pizzas.py
    try it yourself exercises 4-1 and 4-2

---
+++ pizzas.py
@@ -0,0 +1,11 @@
+# 4-1 pizzas
+pizzas = ['pepperoni', 'margherita', 'hawaiian']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
+
+# 4-2 animals
+animals = ['dog', 'cat', 'rabbit']
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet!")
